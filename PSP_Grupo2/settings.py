01b1920107ff8e9a7ffc6a47d5595d97e1c87b9e
TMotax23 <tomasmotasanchez14@gmail.com>
2022-01-12 11:23:42 +0100

    Update 0.2 PSP_Grupo2_Django: Integrated Template

--- PSP_Grupo2/settings.py
+++ PSP_Grupo2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'PSP_Grupo2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['PSP_Grupo2/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 
